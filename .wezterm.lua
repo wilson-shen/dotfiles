9665626b9ca50adda3049e73c46da710705d3db3
Wilson Wong <56531566+wilson-shen@users.noreply.github.com>
2024-08-21 22:29:09 +0800

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -21,68 +21,49 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.font = wezterm.font("CodeNewRoman Nerd Font Mono")
-config.font_size = 12.0
-config.color_scheme = "Monokai (dark) (terminal.sexy)"
-config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
-config.integrated_title_button_style = "Gnome"
--- config.integrated_title_button_style = "MacOsNative"
-config.audible_bell = "Disabled"
-config.window_close_confirmation = "NeverPrompt"
-config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
-}
-
--- keymaps
-config.disable_default_key_bindings = false
-config.keys = {
-	{
-		key = "C",
-		mods = "CTRL",
-		action = wezterm.action.CopyTo("ClipboardAndPrimarySelection"),
-	},
-	{
-		key = "V",
-		mods = "CTRL",
-		action = wezterm.action.PasteFrom("Clipboard"),
-	},
-	{
-		key = "t",
-		mods = "CTRL|ALT",
-		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
-	},
-	{
-		key = "w",
-		mods = "CTRL|ALT",
-		action = wezterm.action.CloseCurrentTab({ confirm = false }),
-	},
-}
-
-for i = 1, 9 do
-	table.insert(config.keys, {
-		key = tostring(i),
-		mods = "CTRL|ALT",
-		action = wezterm.action.ActivateTab(i - 1),
-	})
-end
-
--- background image
-config.background = {
-	{
-		source = {
-			File = "/home/shen/Desktop/SHEN/images/pokemon.gif",
+config = {
+	automatically_reload_config = true,
+	audible_bell = "Disabled",
+	background = {
+		{
+			source = {
+				File = "/home/shen/Desktop/SHEN/images/pokemon.gif",
+			},
+			repeat_x = "NoRepeat",
+			repeat_y = "NoRepeat",
+			hsb = {
+				hue = 1.0,
+				saturation = 1.0,
+				brightness = 0.15,
+			},
+			horizontal_align = "Center",
 		},
-		repeat_x = "NoRepeat",
-		repeat_y = "NoRepeat",
-		hsb = {
-			hue = 1.0,
-			saturation = 1.0,
-			brightness = 0.15,
+	},
+	color_scheme = "Monokai (dark) (terminal.sexy)",
+	disable_default_key_bindings = true,
+	enable_tab_bar = true,
+	font = wezterm.font("CodeNewRoman Nerd Font Mono"),
+	font_size = 12.0,
+  integrated_title_button_style = "Gnome",
+	keys = {
+		{
+			key = "C",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.CopyTo("ClipboardAndPrimarySelection"),
 		},
-    horizontal_align = "Center",
+		{
+			key = "V",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.PasteFrom("Clipboard"),
+		},
+	},
+	window_close_confirmation = "NeverPrompt",
+	window_decorations = "INTEGRATED_BUTTONS|RESIZE",
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
 	},
 }
 
